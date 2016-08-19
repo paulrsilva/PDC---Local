2956970dacbd2a4af5260342786e4b7d3bc5634a
paulrsilva <paulino.rsilva@gmail.com>
2016-08-19 01:40:45 -0300

    Adicionado o helper url do codeigniter, e o <?php echo base_url(); ?> para acertar a localização de imagens/scripts

--- inc/template_scripts.php
+++ inc/template_scripts.php
@@ -7,6 +7,7 @@
  * All vital JS scripts are included here
  *
  */
+ $this->load->helper('url');
 ?>
 
 <!-- Include Jquery library from Google's CDN but if something goes wrong get Jquery from local file (Remove 'http:' if you have SSL) -->
@@ -14,6 +15,6 @@
 <script>!window.jQuery && document.write(decodeURI('%3Cscript src="js/vendor/jquery-1.11.1.min.js"%3E%3C/script%3E'));</script>
 
 <!-- Bootstrap.js, Jquery plugins and Custom JS code -->
-<script src="js/vendor/bootstrap.min.js"></script>
-<script src="js/plugins.js"></script>
-<script src="js/app.js"></script>
+<script src="<?php echo base_url(); ?>js/vendor/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>js/plugins.js"></script>
+<script src="<?php echo base_url(); ?>js/app.js"></script>
