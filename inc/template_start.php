1c6f091b047f566467d0065ed31f10d118af0112
paulrsilva <paulino.rsilva@gmail.com>
2016-08-19 01:40:51 -0300

    Adicionado o helper url do codeigniter, e o <?php echo base_url(); ?> para acertar a localização de imagens/scripts

--- inc/template_start.php
+++ inc/template_start.php
@@ -7,6 +7,7 @@
  * The first block of code used in every page of the template
  *
  */
+ $this->load->helper('url');
 ?>
 <!DOCTYPE html>
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
@@ -25,35 +26,37 @@
 
         <!-- Icons -->
         <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
-        <link rel="shortcut icon" href="images/favicon.ico">
-        <link rel="apple-touch-icon" href="images/icon57.png" sizes="57x57">
-        <link rel="apple-touch-icon" href="images/icon72.png" sizes="72x72">
-        <link rel="apple-touch-icon" href="images/icon76.png" sizes="76x76">
-        <link rel="apple-touch-icon" href="images/icon114.png" sizes="114x114">
-        <link rel="apple-touch-icon" href="images/icon120.png" sizes="120x120">
-        <link rel="apple-touch-icon" href="images/icon144.png" sizes="144x144">
-        <link rel="apple-touch-icon" href="images/icon152.png" sizes="152x152">
+        <link rel="shortcut icon" href="<?php echo base_url(); ?>images/favicon.ico">
+        <link rel="apple-touch-icon" href="<?php echo base_url(); ?>images/icon57.png" sizes="57x57">
+        <link rel="apple-touch-icon" href="<?php echo base_url(); ?>images/icon72.png" sizes="72x72">
+        <link rel="apple-touch-icon" href="<?php echo base_url(); ?>images/icon76.png" sizes="76x76">
+        <link rel="apple-touch-icon" href="<?php echo base_url(); ?>images/icon114.png" sizes="114x114">
+        <link rel="apple-touch-icon" href="<?php echo base_url(); ?>images/icon120.png" sizes="120x120">
+        <link rel="apple-touch-icon" href="<?php echo base_url(); ?>images/icon144.png" sizes="144x144">
+        <link rel="apple-touch-icon" href="<?php echo base_url(); ?>images/icon152.png" sizes="152x152">
         <!-- END Icons -->
 
         <!-- Stylesheets -->
         <!-- Bootstrap is included in its original form, unaltered -->
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- Adicionei o base_url nos links para trabalhar com o codeigniter -->
+        
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.min.css">
 
         <!-- Related styles of various icon packs and plugins -->
-        <link rel="stylesheet" href="css/plugins.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/plugins.css">
 
         <!-- The main stylesheet of this template. All Bootstrap overwrites are defined in here -->
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/main.css">
 
         <!-- Include a specific file here from css/themes/ folder to alter the default theme of the template -->
-        <?php if ($template['theme']) { ?><link id="theme-link" rel="stylesheet" href="css/themes/<?php echo $template['theme']; ?>.css"><?php } ?>
+        <?php if ($template['theme']) { ?><link id="theme-link" rel="stylesheet" href="<?php echo base_url(); ?>css/themes/<?php echo $template['theme']; ?>.css"><?php } ?>
 
         <!-- The themes stylesheet of this template (for using specific theme color in individual elements - must included last) -->
-        <link rel="stylesheet" href="css/themes.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/themes.css">
         <!-- END Stylesheets -->
 
         <!-- Modernizr (browser feature detection library) & Respond.js (Enable responsive CSS code on browsers that don't support it, eg IE8) -->
-        <script src="js/vendor/modernizr-2.7.1-respond-1.4.2.min.js"></script>
+        <script src="<?php echo base_url(); ?>js/vendor/modernizr-2.7.1-respond-1.4.2.min.js"></script>
     </head>
     <!-- In the PHP version you can set the following options from inc/config file -->
     <!--
